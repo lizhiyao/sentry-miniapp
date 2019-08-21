4ad173a71830da3f5cea8d2f414e7dc870ba8128
李志遥 <dancerlzy@gmail.com>
2019-08-21 10:05:58 +0800

    chore: 测试可以上报头条小程序 onError 捕获的异常

--- examples/ttapp/app.js
+++ examples/ttapp/app.js
@@ -9,7 +9,30 @@ Sentry.init({
 
 App({
   onLaunch: function () {
-    myUndefinedFunction();
+    // myUndefinedFunction();
+  },
+  // 头条暂时不支持使用 async
+  // async onShow() {
+  //   // 测试 async 函数中异常是否可以被 onError 捕获 ---> onError 无法捕获
+  //   const ret = await new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve('this is await ret.');
+  //     }, 2000);
+  //   });
+  //   console.log(ret);
+  //   myrUndefinedFunctionInAsyncFunction();
+  // },
+  onShow() {
+    // 测试 Promise 中异常是否可以被 onError 捕获 ---> onError 无法捕获
+    const ret = new Promise((resolve) => {
+      myrUndefinedFunctionInAsyncFunction();
+    })
+    // .then((res) => {
+    //   console.log(res);
+    // }, (error) => {
+    //   console.log(error);
+    //   Sentry.captureException(error);
+    // })
   },
   onError(error) {
     console.warn('onError', error);
