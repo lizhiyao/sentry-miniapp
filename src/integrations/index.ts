c02ac15ec7bb83b56a62ecc0f3e648dda8233200
lizhiyao <dancerlzy@gmail.com>
2020-04-20 16:52:05 +0800

    chore: 移除 UserAgent 插件

--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -2,7 +2,6 @@ export { GlobalHandlers } from "./globalhandlers";
 export { TryCatch } from "./trycatch";
 export { Breadcrumbs } from "./breadcrumbs";
 export { LinkedErrors } from "./linkederrors";
-export { UserAgent } from "./useragent";
 
 export { System } from "./system";
 export { Router } from "./router";
