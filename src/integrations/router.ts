5a8ab3bfa52a0dc789c9f2f950d5cea4c728abe9
lizhiyao <dancerlzy@gmail.com>
2020-07-21 23:15:06 +0800

    feat: Router 插件增加 enable 选项

--- src/integrations/router.ts
+++ src/integrations/router.ts
@@ -3,6 +3,11 @@ import { Event, Integration } from "@sentry/types";
 
 declare const getCurrentPages: any;
 
+/** JSDoc */
+interface RouterIntegrations {
+  enable?: boolean;
+}
+
 /** UserAgent */
 export class Router implements Integration {
   /**
@@ -15,13 +20,26 @@ export class Router implements Integration {
    */
   public static id: string = "Router";
 
+  /** JSDoc */
+  private readonly _options: RouterIntegrations;
+
+  /**
+   * @inheritDoc
+   */
+  public constructor(options?: RouterIntegrations) {
+    this._options = {
+      enable: true,
+      ...options,
+    };
+  }
+
   /**
    * @inheritDoc
    */
   public setupOnce(): void {
     addGlobalEventProcessor((event: Event) => {
       if (getCurrentHub().getIntegration(Router)) {
-        if (getCurrentPages) {
+        if (this._options.enable) {
           try {
             const routers = getCurrentPages().map(
               (route: { route: string; options: object }) => ({
