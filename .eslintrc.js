d1b882024d52bd8883f17cd15b40840408cd7b72
lizhiyao <dancerlzy@gmail.com>
2024-10-19 21:23:42 +0800

    feat: 更新项目依赖及版本

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+// ESLint config docs: https://eslint.org/docs/user-guide/configuring/
+
+module.exports = {
+  root: true,
+  env: {
+    es2017: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: ['@sentry-internal/eslint-config-sdk'],
+  ignorePatterns: [
+    'coverage/**',
+    'build/**',
+    'dist/**',
+    'cjs/**',
+    'esm/**',
+    'examples/**',
+    'test/manual/**',
+    'types/**',
+  ],
+  reportUnusedDisableDirectives: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: ['tsconfig.json'],
+      },
+    },
+    {
+      files: ['test/**/*.ts', 'test/**/*.tsx'],
+      parserOptions: {
+        project: ['tsconfig.test.json'],
+      },
+    },
+    {
+      files: ['jest/**/*.ts', 'scripts/**/*.ts'],
+      parserOptions: {
+        project: ['tsconfig.dev.json'],
+      },
+    },
+    {
+      files: ['*.tsx'],
+      rules: {
+        // Turn off jsdoc on tsx files until jsdoc is fixed for tsx files
+        // See: https://github.com/getsentry/sentry-javascript/issues/3871
+        'jsdoc/require-jsdoc': 'off',
+      },
+    },
+    {
+      files: ['scenarios/**', 'dev-packages/rollup-utils/**', 'dev-packages/bundle-analyzer-scenarios/**'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+      rules: {
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['vite.config.ts'],
+      parserOptions: {
+        project: ['tsconfig.test.json'],
+      },
+    },
+  ],
+};
